543450764253be80fdd95f524756cef01fc72d38
Nerto Kageh <kagehnerto@yahoo.com>
2022-11-23 14:59:38 -0600

    Completed Hackwich and fixed errors.

--- Roll the Dice Hackwich/ContentView.swift
+++ Roll the Dice Hackwich/ContentView.swift
@@ -10,8 +10,6 @@ import SwiftUI
 struct ContentView: View {
     @State private var randomValue = 0
     @State private var rotation = 0.0
-    
-    
     var body: some View {
         VStack {
             Text("Dice Roll")
@@ -24,28 +22,29 @@ struct ContentView: View {
                 .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
                 .padding()
                 .onTapGesture {
+                    chooseRandom(times: 3)
                     withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
                         rotation += 360
                     }
-                    randomValue = Int.random(in:1...6)
                 }
+            Spacer()
         }
-        Spacer()
     }
+    
     func chooseRandom(times:Int) {
         if times > 0 {
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                 randomValue = Int.random(in: 1...6)
-                chooseRandom(times: 3)
+                chooseRandom(times: times - 1)
             }
         }
     }
-    
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
+
 
