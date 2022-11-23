22ca3c568221bc497b67208e02e149cd33f04398
Nerto Kageh <kagehnerto@yahoo.com>
2022-11-23 13:03:40 -0600

    Initial Commit

---
+++ Roll the Dice Hackwich/Roll_the_Dice_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_Dice_HackwichApp.swift
+//  Roll the Dice Hackwich
+//
+//  Created by Induran Aderinto  on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_Dice_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
